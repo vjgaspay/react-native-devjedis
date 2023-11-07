f3b00546df486ccaf57a62abed47cd106a19867d
Vincent John Gaspay <vjgaspay@gmail.com>
2023-11-07 19:51:24 +0800

    Lesson 8: Toggle switch

--- src/app/user/login/index.js
+++ src/app/user/login/index.js
@@ -1,13 +1,33 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, Switch } from "react-native";
 import { Link } from "expo-router";
 
 const Login = () => {
+    const [ showName, setShowName ] = useState(true);
+    const toggleSwitch = () => {
+        setShowName( (prevVal) => !prevVal );
+    }
+
     return (
         <View>
             <Text>Login Screen</Text>
             <Link href="/user/login/forgot-password" style={{marginTop: 15, color: 'blue'}}>Forgot Password</Link>
 
             <Link href="/user/login/modal" style={{marginTop: 15, color: 'blue'}}>Show Modal</Link>
+
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Switch
+                    value={showName}
+                    onValueChange={toggleSwitch}
+                    trackColor={{ false: '#dcdcdc', true: '#0d6efd' }}
+                    thumbColor={ showName ? 'white' : 'white' }
+                    ios_backgroundColor="#dcdcdc"
+                />
+                <Text style={{ fontSize: 18, paddingLeft: 10 }}>Show name</Text>
+            </View>
+
+            { showName && <Text style={{marginTop: 15, fontSize: 25}}>{ process.env.EXPO_PUBLIC_MY_NAME }</Text> }
+            
         </View>
     );
 }
